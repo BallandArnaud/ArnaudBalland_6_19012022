45e6b02557990ec10013e4f0750fbfa8ad87c77f
Arnaud Balland <balland.arnaud@gmail.com>
2022-04-18 15:42:01 +0200

    Ajout éléments focusable

--- scripts/templates/PhotographerInformations.js
+++ scripts/templates/PhotographerInformations.js
@@ -11,12 +11,12 @@ export class PhotographerInformation {
   renderHeader () {
     return `
         <div class="photograph-header__informations">
-            <h1 class="photograph-header__name">${this.name}</h1>
-            <p class="photograph-header__location">${this.city}, ${this.country}</p>
+            <h1 class="photograph-header__name" tabindex="0">${this.name}</h1>
+            <p class="photograph-header__location" tabindex="0">${this.city}, ${this.country}</p>
             <p class="photograph-header__tagline">${this.tagline}</p>
         </div>
         <button id="contactBtn" class="btn btn--contact" tabindex="0">Contactez-moi</button>
-        <img class="photograph-header__picture" src="assets/photographers/${this.picture}" alt="${this.name}">
+        <img class="photograph-header__picture" src="assets/photographers/${this.picture}" alt="Photo de profil de ${this.name}" tabindex="0">
         `
   }
 
